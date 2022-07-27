e8fa5e09ec9a029f3fb20abae5221c787320142d
Finsch <ichbinfenris@gmail.com>
2022-07-28 01:41:42 +0700

    done tablet

--- src/comps/Athlete.js
+++ src/comps/Athlete.js
@@ -5,16 +5,19 @@ import { Grid } from '@mui/material';
 
 import football from '../assets/football.png';
 
+import AthleteImg from './AthleteImg';
+
 export default function Athlete({ athlete }) {
     console.log("🚀 ~ file: Athlete.js ~ line 7 ~ Athlete ~ athlete", athlete)
     return (
         <Box sx={{position:'relative'}}>
-            <img src={football} alt="football" style={{position:'absolute', top:20, left:'10%', width:'35%', height:'auto'}}/>
+            { /* <img src={football} alt="football" style={{position:'absolute', top:20, left:'10%', width:'35%', height:'auto'}}/> */ }
+            <AthleteImg />
             <Box sx={{ minHeight: 100, background: 'white', display: 'flex', justifyContent: 'flex-end', py:7 }}>
                 <Grid container spacing={0}>
-                    <Grid item xs={12} md={6}></Grid>
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{width:{md:'70%'}}}>     
+                    <Grid item xs={12} sm={4} md={6}></Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                    <Box sx={{width:{sm:'80%', md:'70%'}, pl:{sm:5}}}>     
                         <Typography variant="h1" sx={{ fontSize: '90px', color: '#E7E7E7' }}>ATHLETS</Typography>
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px',borderBottom:'5px solid #603EBE',mr:1 }}>01</Typography>
@@ -30,9 +33,9 @@ export default function Athlete({ athlete }) {
             </Box>
             <Box sx={{ minHeight: 100, background: '#F5F4F9', display: 'flex', justifyContent: 'flex-end', py:7 }}>
                 <Grid container spacing={0}>
-                    <Grid item xs={12} md={6}></Grid>
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{width:{md:'70%'}}}>     
+                    <Grid item xs={12} sm={4} md={6}></Grid>
+                    <Grid item xs={12} sm={8} md={6}>
+                    <Box sx={{width:{sm:'80%', md:'70%'}, pl:{sm:5}}}>     
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px',borderBottom:'5px solid #603EBE', mr:1 }}>02</Typography>
                             <Typography variant="h3" sx={{ fontSize: '36px', color: '#C2C2C2' }}>{athlete.subsection[1].title}</Typography>
@@ -47,15 +50,15 @@ export default function Athlete({ athlete }) {
             </Box>
             <Box sx={{ minHeight: 100, background: '#5E3DB3', display: 'flex', justifyContent: 'flex-end', py:7 }}>
                 <Grid container spacing={0}>
-                    <Grid item xs={12} md={6}></Grid>
-                    <Grid item xs={12} md={6}>    
-                    <Box sx={{width:{md:'70%'}}}>            
+                    <Grid item xs={12} sm={4} md={6}></Grid>
+                    <Grid item xs={12} sm={8} md={6}>    
+                    <Box sx={{width:{sm:'80%',md:'70%'}, pl:{sm:5}}}>            
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px', borderBottom:'5px solid white',mr:1 }}>03</Typography>
                             <Typography variant="h3" sx={{ fontSize: '36px', color: '#C2C2C2' }}>{athlete.subsection[2].title}</Typography>
                         </Box>
                         
-                        <Typography variant="body1" sx={{ fontSize: {xs:'15px', sm:'18px',md:'20px'},mt:3 }}>{athlete.subsection[2].description}</Typography>
+                        <Typography variant="body1" sx={{ fontSize: {xs:'15px', sm:'18px',md:'20px'},mt:3, color:'white' }}>{athlete.subsection[2].description}</Typography>
                         </Box>        
                     </Grid>
 
--- src/comps/Players.js
+++ src/comps/Players.js
@@ -5,16 +5,19 @@ import { Grid } from '@mui/material';
 
 import player from '../assets/player.png';
 
+import PlayerImg from './PlayerImg';
+
 export default function Players({ players }) {
     console.log("🚀 ~ file: Athlete.js ~ line 7 ~ Athlete ~ athlete", players)
     return (
         <Box sx={{position:'relative'}}>
-            <img src={player} alt="player" style={{position:'absolute', top:20, right:'10%', width:'35%', height:'auto'}}/>
+            { /* <img src={player} alt="player" style={{position:'absolute', top:20, right:'10%', width:'35%', height:'auto'}}/> */}
+            <PlayerImg />
             <Box sx={{ minHeight: 100, background: 'white', display: 'flex', justifyContent: 'flex-end', py:7 }}>
                 <Grid container spacing={0}>
                     
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{width:{md:'70%', float:'right'}}}>     
+                    <Grid item xs={12} sm={10} md={6}>
+                    <Box sx={{width:{sm:'90%',md:'70%', float:'right'}}}>     
                         <Typography variant="h1" sx={{ fontSize: '90px', color: '#E7E7E7' }}>PLAYERS</Typography>
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px',borderBottom:'5px solid #603EBE',mr:1 }}>01</Typography>
@@ -24,15 +27,15 @@ export default function Players({ players }) {
                         <Typography variant="body1" sx={{ fontSize: {xs:'15px', sm:'18px',md:'20px'},mt:3 }}>{players.subsection[0].description}</Typography>
                     </Box>
                     </Grid>
-                    <Grid item xs={12} md={6}></Grid>
+                    <Grid item xs={12} sm={2} md={6}></Grid>
                 </Grid>
 
             </Box>
             <Box sx={{ minHeight: 100, background: '#F5F4F9', display: 'flex', justifyContent: 'flex-end', py:7 }}>
                 <Grid container spacing={0}>
                     
-                    <Grid item xs={12} md={6}>
-                    <Box sx={{width:{md:'70%', float:'right'}}}>     
+                    <Grid item xs={12} sm={10} md={6}>
+                    <Box sx={{width:{sm:'90%',md:'70%', float:'right'}}}>     
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px',borderBottom:'5px solid #603EBE', mr:1 }}>02</Typography>
                             <Typography variant="h3" sx={{ fontSize: '36px', color: '#C2C2C2' }}>{players.subsection[1].title}</Typography>
@@ -41,15 +44,15 @@ export default function Players({ players }) {
                         <Typography variant="body1" sx={{ fontSize: {xs:'15px', sm:'18px',md:'20px'},mt:3 }}>{players.subsection[1].description}</Typography>
                     </Box>
                     </Grid>
-                    <Grid item xs={12} md={6}></Grid>
+                    <Grid item xs={12} sm={2} md={6}></Grid>
                 </Grid>
 
             </Box>
-            <Box sx={{ minHeight: 100, background: '#090C35', display: 'flex', justifyContent: 'flex-end', py:7 }}>
+            <Box sx={{ minHeight: 100, background: '#090C35', display: 'flex', justifyContent: 'flex-start', py:7 }}>
                 <Grid container spacing={0}>
                     
-                    <Grid item xs={12} md={6}>    
-                    <Box sx={{width:{md:'70%', float:'right'}}}>            
+                    <Grid item xs={12} sm={10} md={6}>    
+                    <Box sx={{width:{sm:'90%',md:'70%', float:'right'}}}>            
                         <Box sx={{display: 'flex', alignItems:'center'}}>
                             <Typography variant="caption" sx={{ fontSize: '40px',lineHeight:'60px', borderBottom:'5px solid white',mr:1, color:'#8F6BE8' }}>03</Typography>
                             <Typography variant="h3" sx={{ fontSize: '36px', color: '#C2C2C2' }}>{players.subsection[2].title}</Typography>
@@ -58,7 +61,7 @@ export default function Players({ players }) {
                         <Typography variant="body1" sx={{ fontSize: {xs:'15px', sm:'18px',md:'20px'},mt:3, color:'white' }}>{players.subsection[2].description}</Typography>
                         </Box>        
                     </Grid>
-                    <Grid item xs={12} md={6}></Grid>
+                    <Grid item xs={12} sm={2} md={6}></Grid>
                 </Grid>
 
             </Box>
